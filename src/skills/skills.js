3c23a4d0390566c5cd9ad4e4ef18ceb5f7945234
Hwajiin <borderline0px@gmail.com>
2021-09-09 19:36:57 +0900

    skills section edit scroll event process

--- src/skills/skills.js
+++ src/skills/skills.js
@@ -19,9 +19,9 @@ class Skills {
     // 어디에 맞춰서 트랜지션 제거할지 고민,,,
 
     if (scrollY >= scrollTop - HEADER_HEIGHT) {
-      this.box.style.transform = `scale(1) translateY(0px)`;
+      this.box.classList.add("active");
     } else if (scrollY < HEADER_HEIGHT) {
-      this.box.style.transform = `scale(0.8) translateY(30px)`;
+      this.box.classList.remove("active");
     }
   };
 }
