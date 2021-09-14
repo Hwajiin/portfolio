b5ff428bf2d584a988f2ac62c962c9244193da53
Hwajiin <borderline0px@gmail.com>
2021-09-14 16:56:17 +0900

    projects section: make project detail part, styled and added click event

--- src/projects/projects.js
+++ src/projects/projects.js
@@ -1,28 +1,86 @@
 class Projects {
   constructor() {
     this.projects = document.querySelectorAll(".projects__item");
+    this.detailBackground = document.querySelector(".detail__background");
+    this.closeBtn = document.querySelector(".close-btn");
+    this.detailData = this.detailBackground.querySelector(".detail__data");
+    this.projectData;
     this.setEventListener();
+    this.fetchData();
   }
 
   setEventListener = () => {
     this.projects.forEach((project) =>
       project.addEventListener("mouseenter", this.removeFilter)
     );
+
     this.projects.forEach((project) =>
       project.addEventListener("mouseleave", this.addFilter)
     );
+
+    this.projects.forEach((project) =>
+      project.addEventListener("click", this.showDetail)
+    );
+
+    this.closeBtn.addEventListener("click", () => {
+      this.detailBackground.classList.remove("showDetail");
+    });
   };
 
   removeFilter = (e) => {
-    const project = e.target;
+    const project = e.currentTarget;
     const thumbnail = project.querySelector(".projects__item__thumbnail");
     thumbnail.classList.add("colorIn");
   };
 
   addFilter = (e) => {
-    const project = e.target;
+    const project = e.currentTarget;
     const thumbnail = project.querySelector(".projects__item__thumbnail");
     thumbnail.classList.remove("colorIn");
+  };
+
+  showDetail = async (e) => {
+    const project = e.currentTarget;
+    if (!this.detailBackground.classList.contains("showDetail")) {
+      this.detailBackground.classList.add("showDetail");
+    }
+
+    const target = this.projectData.projects.find(
+      (item) => item.title === project.dataset.title
+    );
+
+    // const detailThumbnail = document.createElement("div");
+    // detailThumbnail.setAttribute("class", "detail__thumbnail");
+    // this.detailData.appendChild(detailThumbnail);
+
+    const html = this.createHTML(target);
+    this.detailData.innerHTML = html;
+
+    const thumbnail = this.detailData.querySelector(".detail__thumbnail");
+    if (thumbnail) {
+      thumbnail.style.backgroundImage = `url(${target.image})`;
+    }
+  };
+
+  createHTML = (data) => {
+    return `
+      <div class="detail__thumbnail"></div>
+      <div class="detail__metadata">
+        <h1 class="detail__title">${data.title}</h1>
+        <p>${data.since}</p>
+        <button class="deploy">${data.path}</button>
+      </div>
+    `;
+  };
+
+  getData = async () => {
+    const res = await fetch("src/data/project_data.json");
+    const data = await res.json();
+    return data;
+  };
+
+  fetchData = async () => {
+    this.projectData = await this.getData();
   };
 }
 
